ca39006b41f5be359e311fc19e679bad6cc057a2
Craig H <70126287+cxhx441@users.noreply.github.com>
2023-08-03 10:23:43 -0700

    testing

---
+++ test_insertion_loss.py
@@ -0,0 +1,14 @@
+import unittest
+from InsertionLoss import InsertionLoss
+from Source import Source
+from Receiver import Receiver
+from Barrier import Barrier
+
+
+class TestInsertionLoss(unittest.TestCase):
+    def test_1(self):
+        self.assertTrue(True)
+
+
+if __name__ == "__main__":
+    unittest.main()
