4890456e27e3f101179826545794acb0d36b375c
Craig H <70126287+cxhx441@users.noreply.github.com>
2024-03-01 11:25:26 -0800

    add more attributes to Source, Receiver, Barrier

--- Receiver.py
+++ Receiver.py
@@ -2,8 +2,14 @@ from sympy import Point
 
 
 class Receiver():
-    def __init__(self, geo: Point) -> None:
+    def __init__(self, geo: Point,
+                 name: str = None,
+                 dBA_limit: float = None,
+                 dBA_predictied: float = None
+                ) -> None:
         self.geo = geo
         self.affecting_sources = set()  # set of sources that affect this receiver
         self.source_barrier_pairs = set()  # set of tuples of (source, barrier) pairs.
-        self.dBA = 0  # the dBA level at this receiver
+        self.name = name
+        self.dBA_limit = dBA_limit
+        self.dBA_predicted = dBA_limit
