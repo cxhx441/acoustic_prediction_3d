05fa68f6d6fb3990cb247741377dccd8df6096c5
Craig H <70126287+cxhx441@users.noreply.github.com>
2024-04-19 20:48:10 -0700

    reformat

--- Barrier.py
+++ Barrier.py
@@ -2,8 +2,8 @@ from sympy import Segment
 
 class Barrier:
     def __init__(self,
-                 geo: Segment,
-                 name: str = None
+                 geo:   Segment,
+                 name:  str = None
                 ) -> None:
         self.geo = geo
         self.name = name
--- Receiver.py
+++ Receiver.py
@@ -1,13 +1,14 @@
 from sympy import Point
 
+
 class Receiver():
     def __init__(self,
-                 geo:           Point,
-                 name:          str =   None,
-                 dBA_limit:     float = None,
-                 dBA_predicted: float = None
-                ) -> None:
-        self.geo =              geo
-        self.name =             name
-        self.dBA_limit =        dBA_limit
-        self.dBA_predicted =    dBA_predicted
+                 geo: Point,
+                 name:              str     = None,
+                 dBA_limit:         float   = None,
+                 dBA_predicted:     float   = None
+                 ) -> None:
+        self.geo            = geo
+        self.name           = name
+        self.dBA_limit      = dBA_limit
+        self.dBA_predicted  = dBA_predicted
