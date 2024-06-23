3ac2ed64772e169344ebf1c419303ab1fb361e33
Craig H <70126287+cxhx441@users.noreply.github.com>
2024-06-23 16:20:39 -0700

    signals with buttons and main window

--- gui.py
+++ gui.py
@@ -2,27 +2,49 @@ from PyQt6.QtCore import QSize, Qt
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 
+import sys
+from random import choice
+
+window_titles = [
+    'My App',
+    'My App',
+    'Still My App',
+    'Still My App',
+    'What on earth',
+    'What on earth',
+    'This is surprising',
+    'This is surprising',
+    'Something went wrong'
+]
+
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        button = QPushButton("Press Me")
-        button.setCheckable(True)
-        button.clicked.connect(self.button_clicked)
-        button.clicked.connect(self.button_toggled)
+        self.n_times_clicked = 0
 
-        self.setWindowTitle("Sound Map")
-        # self.setFixedSize(QSize(400, 300))
-        # self.setMinimumSize(QSize(400, 300))
-        self.setMaximumSize(QSize(400, 300))
-        self.setCentralWidget(button)
+        self.setWindowTitle("My App")
 
-    def button_clicked(self):
-        print("clicked")
-    def button_toggled(self, checked):
-        print("checked?", checked)
+        self.button = QPushButton("Press Me!")
+        self.button.clicked.connect(self.the_button_was_clicked)
 
+        self.windowTitleChanged.connect(self.the_window_title_changed)
 
+        # Set the central widget of the Window.
+        self.setCentralWidget(self.button)
+
+    def the_button_was_clicked(self):
+        print("Clicked.")
+        new_window_title = choice(window_titles)
+        print("Setting title:  %s" % new_window_title)
+        self.setWindowTitle(new_window_title)
+
+    def the_window_title_changed(self, window_title):
+        print("Window title changed: %s" % window_title)
+
+        if window_title == 'Something went wrong':
+            self.button.setDisabled(True)
 # You need one (and only one) QApplication instance per application.
 app = QApplication([])
 
