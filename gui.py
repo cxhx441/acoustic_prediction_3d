c5b384355681d88e445c1bc240cb5b79cc17aa4a
Craig H <70126287+cxhx441@users.noreply.github.com>
2024-06-23 18:14:57 -0700

    nested layouts

--- gui.py
+++ gui.py
@@ -1,67 +1,44 @@
-from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import (
-    QApplication,
-    QCheckBox,
-    QComboBox,
-    QDateEdit,
-    QDateTimeEdit,
-    QDial,
-    QDoubleSpinBox,
-    QFontComboBox,
-    QLabel,
-    QLCDNumber,
-    QLineEdit,
-    QMainWindow,
-    QProgressBar,
-    QPushButton,
-    QRadioButton,
-    QSlider,
-    QSpinBox,
-    QTimeEdit,
-    QVBoxLayout,
-    QWidget,
-)
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout
+from PyQt6.QtGui import QPalette, QColor
 
+class Color(QWidget):
 
-# Subclass QMainWindow to customize your application's main window
+    def __init__(self, color):
+        super(Color, self).__init__()
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        palette.setColor(QPalette.ColorRole.Window, QColor(color))
+        self.setPalette(palette)
+
 class MainWindow(QMainWindow):
+
     def __init__(self):
-        super().__init__()
+        super(MainWindow, self).__init__()
 
-        self.setWindowTitle("Widgets App")
+        self.setWindowTitle("My App")
 
-        layout = QVBoxLayout()
-        widgets = [
-            QCheckBox,
-            QComboBox,
-            QDateEdit,
-            QDateTimeEdit,
-            QDial,
-            QDoubleSpinBox,
-            QFontComboBox,
-            QLCDNumber,
-            QLabel,
-            QLineEdit,
-            QProgressBar,
-            QPushButton,
-            QRadioButton,
-            QSlider,
-            QSpinBox,
-            QTimeEdit,
-        ]
+        layout_h = QHBoxLayout()
+        layout_v1 = QVBoxLayout()
+        layout_v2 = QVBoxLayout()
 
-        for w in widgets:
-            layout.addWidget(w())
+        layout_v1.addWidget(Color('red'))
+        layout_v1.addWidget(Color('yellow'))
+        layout_v1.addWidget(Color('purple'))
+
+        layout_v2.addWidget(Color('orange'))
+        layout_v2.addWidget(Color('blue'))
+        layout_v2.addWidget(Color('green'))
+
+        layout_h.addLayout(layout_v1)
+        layout_h.addLayout(layout_v2)
 
         widget = QWidget()
-        widget.setLayout(layout)
-
-        # Set the central widget of the Window. Widget will expand
-        # to take up all the space in the window by default.
+        widget.setLayout(layout_h)
         self.setCentralWidget(widget)
 
-
 app = QApplication([])
+
 window = MainWindow()
 window.show()
 
