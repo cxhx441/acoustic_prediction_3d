3eaa363e7ff511100367079725dc82f998ebc3a1
Craig H <70126287+cxhx441@users.noreply.github.com>
2024-06-24 16:47:42 -0700

    additional windows (creating window)

--- gui.py
+++ gui.py
@@ -1,31 +1,40 @@
+from random import random, randint
 
-from PyQt6.QtWidgets import QApplication, QDialog, QMainWindow, QPushButton, QDialogButtonBox, QVBoxLayout, QLabel, \
-    QMessageBox
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget
+
+import sys
+
+
+class AnotherWindow(QWidget):
+    """
+    This "window" is a QWidget. If it has no parent, it
+    will appear as a free-floating window as we want.
+    """
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+        self.label = QLabel(str(randint(0, 100)))
+        layout.addWidget(self.label)
+        self.setLayout(layout)
 
 
 class MainWindow(QMainWindow):
+
     def __init__(self):
         super().__init__()
+        self.w = None
+        self.button = QPushButton("Push for Window")
+        self.button.clicked.connect(self.show_new_window)
+        self.setCentralWidget(self.button)
 
-        self.setWindowTitle("My App")
+    def show_new_window(self, checked):
+        if self.w is None:
+            self.w = AnotherWindow()
+            self.w.show()
 
-        button = QPushButton("Press me for a dialog!")
-        button.clicked.connect(self.button_clicked)
-        self.setCentralWidget(button)
-
-    def button_clicked(self, s):
-
-        button = QMessageBox.question(self, "Question dialog", "The longer message")
-        # QMessageBox.about(parent, title, message)
-        # QMessageBox.critical(parent, title, message)
-        # QMessageBox.information(parent, title, message)
-        # QMessageBox.question(parent, title, message)
-        # QMessageBox.warning(parent, title, message)
-
-        if button == QMessageBox.StandardButton.Yes:
-            print("Yes!")
         else:
-            print("No!")
+            self.w.close()  # Close window.
+            self.w = None  # Discard reference.app = QApplication([])
 
 app = QApplication([])
 w = MainWindow()
