883ad26aa649186a14e2e9c1bdce6901bd30a7c8
Craig H <70126287+cxhx441@users.noreply.github.com>
2024-06-24 16:13:02 -0700

    better Qdialog

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 
-from PyQt6.QtWidgets import QApplication, QDialog, QMainWindow, QPushButton
+from PyQt6.QtWidgets import QApplication, QDialog, QMainWindow, QPushButton, QDialogButtonBox, QVBoxLayout, QLabel
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -12,11 +13,31 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(button)
 
     def button_clicked(self, s):
-        print("click", s)
+        dlg = CustomDialog()
+        if dlg.exec():
+            print("Success!")
+        else:
+            print("Cancel!")
 
-        dlg = QDialog(self)
-        dlg.setWindowTitle("HELLO!")
-        dlg.exec()
+
+class CustomDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("HELLO!")
+
+        buttons = QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel  # | Ok | Open | Save
+
+        self.buttonBox = QDialogButtonBox(buttons)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
+        self.layout = QVBoxLayout()
+        message = QLabel("Something happened, is that OK?")
+        self.layout.addWidget(message)
+        self.layout.addWidget(self.buttonBox)
+        self.setLayout(self.layout)
+
 
 app = QApplication([])
 w = MainWindow()
