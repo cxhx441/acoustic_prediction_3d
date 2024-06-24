a2939a7596f702b95694a9c804d475b826c85b97
Craig H <70126287+cxhx441@users.noreply.github.com>
2024-06-24 16:01:29 -0700

    basic Qdialog

--- gui.py
+++ gui.py
@@ -1,15 +1,5 @@
-from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtGui import QAction, QIcon, QKeySequence
-from PyQt6.QtWidgets import (
-    QApplication,
-    QCheckBox,
-    QLabel,
-    QMainWindow,
-    QStatusBar,
-    QToolBar,
-)
 
-
+from PyQt6.QtWidgets import QApplication, QDialog, QMainWindow, QPushButton
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -17,57 +7,17 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("My App")
 
-        label = QLabel("Hello!")
+        button = QPushButton("Press me for a dialog!")
+        button.clicked.connect(self.button_clicked)
+        self.setCentralWidget(button)
 
-        # The `Qt` namespace has a lot of attributes to customize
-        # widgets. See: http://doc.qt.io/qt-5/qt.html
-        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-        # Set the central widget of the Window. Widget will expand
-        # to take up all the space in the window by default.
-        self.setCentralWidget(label)
-
-        toolbar = QToolBar("My main toolbar")
-        toolbar.setIconSize(QSize(16, 16))
-        self.addToolBar(toolbar)
-
-        button_action = QAction(QIcon("wafer.png"), "&Your button", self)  # & is which key to navigate (like TAP)
-        button_action.setStatusTip("This is your button")
-        button_action.triggered.connect(self.onMyToolBarButtonClick)
-        button_action.setCheckable(True)
-        # You can enter keyboard shortcuts using key names (e.g. Ctrl+p)
-        # Qt.namespace identifiers (e.g. Qt.CTRL + Qt.Key_P)
-        # or system agnostic identifiers (e.g. QKeySequence.StandardKey.Print)
-        button_action.setShortcut(QKeySequence("Ctrl+p"))
-        toolbar.addAction(button_action)
-
-        toolbar.addSeparator()
-
-        button_action2 = QAction(QIcon("wafer.png"), "Your &button2", self)
-        button_action2.setStatusTip("This is your button2")
-        button_action2.triggered.connect(self.onMyToolBarButtonClick)
-        button_action2.setCheckable(True)
-        toolbar.addAction(button_action2)
-
-        toolbar.addWidget(QLabel("Hello"))
-        toolbar.addWidget(QCheckBox())
-
-        self.setStatusBar(QStatusBar(self))  # bit at the bottom
-
-        menu = self.menuBar()  # file, edit, etc
-
-        # can add QActions so buttons and icons and hotkeys all link to same thing easily
-        file_menu = menu.addMenu("&File")
-        file_menu.addAction(button_action)
-
-        file_menu.addSeparator()
-
-        file_submenu = file_menu.addMenu("Submenu")
-
-        file_submenu.addAction(button_action2)
-
-    def onMyToolBarButtonClick(self, s):
+    def button_clicked(self, s):
         print("click", s)
+
+        dlg = QDialog(self)
+        dlg.setWindowTitle("HELLO!")
+        dlg.exec()
+
 app = QApplication([])
 w = MainWindow()
 w.show()
