20fa42da0187b9637a9940cf944f9351baac41fc
Craig H <70126287+cxhx441@users.noreply.github.com>
2024-06-24 16:15:15 -0700

    better Qdialog, dialog box opens above parent

--- gui.py
+++ gui.py
@@ -2,6 +2,26 @@
 from PyQt6.QtWidgets import QApplication, QDialog, QMainWindow, QPushButton, QDialogButtonBox, QVBoxLayout, QLabel
 
 
+class CustomDialog(QDialog):
+    def __init__(self, parent=None):
+        """ pass main window as parent to open directly above it"""
+        super().__init__(parent)
+
+        self.setWindowTitle("HELLO!")
+
+        buttons = QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel  # | Ok | Open | Save
+
+        self.buttonBox = QDialogButtonBox(buttons)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
+        self.layout = QVBoxLayout()
+        message = QLabel("Something happened, is that OK?")
+        self.layout.addWidget(message)
+        self.layout.addWidget(self.buttonBox)
+        self.setLayout(self.layout)
+
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -18,26 +38,6 @@ class MainWindow(QMainWindow):
             print("Success!")
         else:
             print("Cancel!")
-
-
-class CustomDialog(QDialog):
-    def __init__(self):
-        super().__init__()
-
-        self.setWindowTitle("HELLO!")
-
-        buttons = QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel  # | Ok | Open | Save
-
-        self.buttonBox = QDialogButtonBox(buttons)
-        self.buttonBox.accepted.connect(self.accept)
-        self.buttonBox.rejected.connect(self.reject)
-
-        self.layout = QVBoxLayout()
-        message = QLabel("Something happened, is that OK?")
-        self.layout.addWidget(message)
-        self.layout.addWidget(self.buttonBox)
-        self.setLayout(self.layout)
-
 
 app = QApplication([])
 w = MainWindow()
