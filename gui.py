13af04311ef89c3f8c6e84f5a7c7b1e20d3a897e
Craig H <70126287+cxhx441@users.noreply.github.com>
2024-06-23 18:40:07 -0700

    qAction with status bar and toolbar + multiple actions

--- gui.py
+++ gui.py
@@ -1,17 +1,20 @@
-from PyQt6.QtWidgets import (
-    QMainWindow, QApplication,
-    QLabel, QToolBar, QStatusBar
-)
+from PyQt6.QtCore import QSize, Qt
 from PyQt6.QtGui import QAction, QIcon
-from PyQt6.QtCore import Qt, QSize
+from PyQt6.QtWidgets import (
+    QApplication,
+    QCheckBox,
+    QLabel,
+    QMainWindow,
+    QStatusBar,
+    QToolBar,
+)
 
 
 class MainWindow(QMainWindow):
-
     def __init__(self):
-        super(MainWindow, self).__init__()
+        super().__init__()
 
-        self.setWindowTitle("My Awesome App")
+        self.setWindowTitle("My App")
 
         label = QLabel("Hello!")
         label.setAlignment(Qt.AlignmentFlag.AlignCenter)
@@ -19,21 +22,30 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(label)
 
         toolbar = QToolBar("My main toolbar")
-        toolbar.setIconSize(QSize(16,16))
+        toolbar.setIconSize(QSize(16, 16))
         self.addToolBar(toolbar)
 
-        button_action = QAction(QIcon("wafer.png"), "Your button", self)
+        button_action = QAction(QIcon("wafer.png"), "&Your button", self)
         button_action.setStatusTip("This is your button")
         button_action.triggered.connect(self.onMyToolBarButtonClick)
         button_action.setCheckable(True)
         toolbar.addAction(button_action)
 
-        self.setStatusBar(QStatusBar(self))
+        toolbar.addSeparator()
 
+        button_action2 = QAction(QIcon("wafer.png"), "Your &button2", self)
+        button_action2.setStatusTip("This is your button2")
+        button_action2.triggered.connect(self.onMyToolBarButtonClick)
+        button_action2.setCheckable(True)
+        toolbar.addAction(button_action2)
+
+        toolbar.addWidget(QLabel("Hello"))
+        toolbar.addWidget(QCheckBox())
+
+        self.setStatusBar(QStatusBar(self))
 
     def onMyToolBarButtonClick(self, s):
         print("click", s)
-
 app = QApplication([])
 w = MainWindow()
 w.show()
