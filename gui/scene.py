0cc73eca898b9b7d38e990f9a453fbdadb3f4f69
cxhx441 <cxhx44@gmail.com>
2025-05-21 20:46:31 -0700

    test success!

--- gui/scene.py
+++ gui/scene.py
@@ -7,8 +7,8 @@ class CustomScene(QGraphicsScene):
         super().__init__(parent)
 
         # load image, and load into canvas
-        pixmap = QPixmap(bed_image_path("bed_image.png"))
         # pixmap = QPixmap(bed_image_path("bed_image.jpg"))
+        pixmap = QPixmap(bed_image_path("bed_image.png"))
 
         self.pixmap_item = QGraphicsPixmapItem(pixmap)
         self.addItem(self.pixmap_item)
